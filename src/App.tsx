2230317efa7f77d66f281c449fa84a32ae4a9158
Juliana Ruby <julianarubycocacola@gmail.com>
2025-12-02 16:19:30 -0300

    criação de componets e css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css";
+import { Heading } from "./components/Heading";
+
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
   console.log("oi");
 
   return (
     <>
-      <h1> Ola mundo</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste suscipit,
         impedit velit consequuntur nostrum at corrupti voluptates magnam dolore
